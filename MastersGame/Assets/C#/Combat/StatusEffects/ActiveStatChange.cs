85b1921b4bd18b25ec495ecbbab99ca53ac9138b
T3DK-5235 <136117132+T3DK-5235@users.noreply.github.com>
2024-10-09 12:48:08 +0100

    Slight update to stat scripts

--- MastersGame/Assets/C#/Combat/StatusEffects/ActiveStatChange.cs
+++ MastersGame/Assets/C#/Combat/StatusEffects/ActiveStatChange.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActiveStatChanged : MonoBehaviour
+public class ActiveStatChange : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    // Start is called before the first frame update
+    private EntityStatEnum statChanged;
+    private int statChangedEffect;
+    private int statChangedDuration;
+    // Start is called before the first frame update
+
+    public ActiveStatChange(EntityStatEnum statChanged, int statChangedEffect, int statChangedDuration)
     {
-        
+        this.statChanged = statChanged;
+        this.statChangedEffect = statChangedEffect;
+        this.statChangedDuration = statChangedDuration;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void updateStatDuration() {
+        //It only gets decremented if it isn't 0, and it is the end of a month
+        statChangedDuration -= 1;
+    }
+
+    public EntityStatEnum getStatChanged() {
+        return statChanged;
+    }
+
+    public int getStatChangedEffect() {
+        return statChangedEffect;
+    }
+
+    public int getStatChangedDuration() {
+        return statChangedDuration;
     }
 }
