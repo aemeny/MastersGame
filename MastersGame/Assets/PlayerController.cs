b85b182e63eac1baa972508c10bb3ba51d2eec2c
Ethan Denham <157436021+GildedAcorn@users.noreply.github.com>
2024-10-15 11:12:48 +0100

    PLAYER MOVEMENT FINALLY!

---
+++ MastersGame/Assets/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private DefaultPlayerActions defaultPlayerActions;
+
+    private InputAction moveAction;
+
+    private Rigidbody rb;
+
+    private float moveSpeed = 6.0f;
+
+    private void Awake()
+    {
+        defaultPlayerActions = new DefaultPlayerActions();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        moveAction = defaultPlayerActions.Player.Move;
+        moveAction.Enable();    
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDir = moveAction.ReadValue<Vector3>();
+
+        transform.Translate(moveDir.x *  moveSpeed * Time.deltaTime, 0, moveDir.z * moveSpeed * Time.deltaTime);
+
+        Debug.Log(moveDir);
+    }
+}
